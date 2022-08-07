7676549368d8f7bdb16ff0e953ad13d4847923d2
Ahmet Ayyildiz <ahmetayyildiz.dev@gmail.com>
2022-08-08 01:29:53 +0300

    Db Context Class ekledim

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<NLayer.Core.Category> Categories { get; set; }
+        public DbSet<NLayer.Core.Product> Products { get; set; }
+        public DbSet<NLayer.Core.ProductFeature> ProductFeatures { get; set; }
+        
+    }
+}
