ac5350cb34c76fbb72a738c81e02b33993ff7c95
Ahmet Ayyildiz <ahmetayyildiz.dev@gmail.com>
2022-08-11 00:28:05 +0300

    service layer is finished

---
+++ NLayer.Core/DTOs/CustomReponseDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomReponseDto<T>
+    {
+        public T Data { get; set; }
+
+        public List<String> Errors { get; set; }
+        [JsonIgnore]
+        public int StatusCode{ get; set; }
+
+        //Factory design pattern  
+        //static factor methods
+        public static CustomReponseDto<T> Success(int statusCode, T data)
+            => new CustomReponseDto<T> { Data = data, Errors = null, StatusCode = statusCode };
+
+        public static CustomReponseDto<T> Success(int statusCode)
+            => new CustomReponseDto<T> { StatusCode = statusCode };
+
+        public static CustomReponseDto<T> Fail(int statusCode , List<String> errors)
+            => new CustomReponseDto<T> {  Errors = errors, StatusCode = statusCode };
+
+        public static CustomReponseDto<T> Fail(int statusCode,string error)
+           => new CustomReponseDto<T> { Errors = new List<string>() { error}, StatusCode = statusCode };
+    }
+}
