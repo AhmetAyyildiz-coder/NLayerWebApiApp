3052374bb6b34793b2602a927117dcd4dafba2ee
Ahmet Ayyildiz <ahmetayyildiz.dev@gmail.com>
2022-08-12 21:52:15 +0300

    Global Exception handler

---
+++ NLayer.API/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using NLayer.Core.DTOs;
+using NLayer.Services.Exceptions;
+using System.Text.Json;
+
+namespace NLayer.API.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UserCustomerException(this IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        _ => 500
+                    };
+                    context.Response.StatusCode = statusCode;
+
+                    var response = CustomReponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+
+
+
+                });
+            });
+        }
+    }
+}
